c15ea407b7426251ae50db5f3b5b80b1be33ba4c
kushagra <kushagramiglani13@gmail.com>
2024-05-28 07:39:16 +0530

    new changes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import numpy as np
+# import sklearn
+# print(sklearn.__version__)
 pipe=pickle.load(open('pipe.pkl','rb'));
 df=pickle.load(open('df.pkl','rb'));
 
@@ -12,7 +14,7 @@ ram=st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 weight=st.number_input('weight of the laptop')
 touchscreen=st.selectbox('Touchscreen',['NO', 'Yes'])
 ips=st.selectbox('IPS',['No','Yes'])
-screen_size=st.number_input('Screen Size')
+screen_size=st.number_input(label='Screen Size',min_value=10)
 resolution=st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3480x2160','3200x1800','2880x1800','2560x1440',
 '2304x1440'])
 cpu=st.selectbox('Brand',df['Cpu brand'].unique())
@@ -35,11 +37,8 @@ if(st.button('Predict Price')):
     
     X_res=int(resolution.split('x')[0])
     Y_res=int(resolution.split('x')[1])
-    try:
-       ppi=((X_res**2) +(Y_res**2))**0.5/screen_size
-       query=np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
-       query=query=query.reshape(1,12)
-       st.title("The predicted price is " +str(int(np.exp(pipe.predict(query)[0]))))
-    except:
-        print("The screen size cannot be zero")
+    ppi=((X_res**2) +(Y_res**2))**0.5/screen_size
+    query=np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+    query=query=query.reshape(1,12)
+    st.title("The predicted price is " +str(int(np.exp(pipe.predict(query)[0]))))
 
